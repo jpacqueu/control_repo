99e5d7e90cfc5cadadc6d590bc66bbea6a256433
jpacqueu <127869174+jpacqueu@users.noreply.github.com>
2023-03-15 14:54:50 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,3 @@
 node default {
-  file {'/root/README':
-  ensure  => file,
-  content => 'This is a readme',
-  owner   => 'root',
-  }
-  user {'testuser puppet':
-  name => testuser_puppet,
-  gid => 52,
-  home => '/home/testpuppet',
-  }
+
 }
